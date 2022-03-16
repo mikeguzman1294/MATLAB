249cffdcfb2a1a1de965662f4c6248148850d208
mikeguzman1294 <mikeguzman1294@gmail.com>
2022-03-16 10:38:34 +0100

    Part 1 Exercise 3 - Code refactoring.

--- Exercises/Exercise3.m
+++ Exercises/Exercise3.m
@@ -1,6 +1,8 @@
-N = 10000 % Number of Points for each Signal
+% All wave forms use the same time vector
+
+N = 10000; % Number of Points for each Signal
 T = 5*10^(-4); % Maximum Duration
-t = linspace(0,T,N)
+t = linspace(0,T,N); % Vector temps
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%% FIRST SIGNAL %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % The sine wave: frequency = 100kHz Amplitude = 0.3 V and a phase= 30°.
@@ -10,7 +12,7 @@ f0 = 10^5;
 Am = 0.3;
 Ph = 30 * pi / 180;
 
-s1 = Am * sin(2*pi*f0*t*Ph)
+s1 = Am * sin(2*pi*f0*t*Ph);
 
 figure(1)
 subplot(2,2,1)
@@ -20,5 +22,10 @@ ylim([-0.4 0.4])
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%% SECOND SIGNAL %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% The sine wave: frequency = 100kHz Amplitude = 0.3 V and a phase= 30°.
+% Amplitude modulated signal given by:
+% Modulating signal: cosin wave with amplitude a=0.1 and frequency fm=5kHz
+% Carrier: sinwave with amplitude A=1 and frequency fc=100kHz
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
