3c9a97e0d4906148976d35d55b4855f03bb764bd
mikeguzman1294 <mikeguzman1294@gmail.com>
2022-03-16 11:05:56 +0100

    Part 2 - Exercise 3 - Amplitude Modulated Signal

--- Exercises/Exercise3.m
+++ Exercises/Exercise3.m
@@ -12,7 +12,7 @@ f0 = 10^5;
 Am = 0.3;
 Ph = 30 * pi / 180;
 
-s1 = Am * sin(2*pi*f0*t*Ph);
+s1 = Am * sin(2*pi*f0*t+Ph);
 
 figure(1)
 subplot(2,2,1)
@@ -26,6 +26,35 @@ ylim([-0.4 0.4])
 % Modulating signal: cosin wave with amplitude a=0.1 and frequency fm=5kHz
 % Carrier: sinwave with amplitude A=1 and frequency fc=100kHz
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% So Modulating signal is defined by:
+% xm(t) = Am * cos (wm * t)
+
+% While Carrier signal is defined by:
+% xp(t) = Ap * cos (wp * t)
+
+% Which gives us the Amplitude Modulating Signal:
+% x(t) = Ap * (1 + Am * cos (wm * t)) * cos (wp * t)
+
+% Definition of modulating signal parameters
+Am = 0.1;
+fm = 5^5;
+
+% Definition of modulating signal parameters
+Ap = 0.1;
+fp = 100^5;
+
+% We define the Amplitude Modulated Signal:
+s2 = Ap * (1 + Am * cos(2*pi*fm * t)) .* cos(2*pi*fp * t);
+
+figure(1)
+subplot(2,2,2)
+plot(t,s2)
+title('AM modulated signal')
+ylim([-0.4 0.4])
+
+
+
 
 
 
